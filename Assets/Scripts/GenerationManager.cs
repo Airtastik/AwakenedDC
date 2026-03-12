41c1321af8909956518e5e3d06ddd61550efb0f6
dmpinyar <dmpinyard@gmail.com>
2026-03-11 22:50:31 -0400

    added room move

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -7,7 +7,6 @@ using System.Linq;
 public class GenerationManager : MonoBehaviour
 {
     public List<RoomSpawner> generatorPrefabs;
-    public GameObject playerInstance;
     private RoomSpawner currentSpawner;
 
     void Start()
@@ -16,14 +15,16 @@ public class GenerationManager : MonoBehaviour
         buildEnviornment(0);
     }
 
-    void buildEnviornment(int idx) {
-        if (currentSpawner != null)
-            currentSpawner.purgeEnviornment();
+    public void buildEnviornment(int idx) {
+        if (currentSpawner != null) {
+            Destroy(currentSpawner.gameObject);
+        }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = new Vector3(0, player.transform.position.y, 0);
+
         currentSpawner = Instantiate(generatorPrefabs[idx], new Vector3(0, 0, 0), Quaternion.identity);
 
-        Vector3 oldPos = playerInstance.transform.position;
-        oldPos.x = 0;
-        oldPos.z = 0;
-        playerInstance.transform.position = oldPos;
+        
     }
 }
