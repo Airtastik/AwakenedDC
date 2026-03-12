b74fcfa8d8d221d31deeff866cce947b42ec18b6
Cole Sanders <cgsanders3@gmail.com>
2026-03-11 22:25:31 -0400

    Added hud functionality

--- Assets/Scripts/Stats/StatsUIManager.cs
+++ Assets/Scripts/Stats/StatsUIManager.cs
@@ -1,3 +1,9 @@
+// Generated using Gemini 3 Fast on 3/11/26
+//Prompts: I have (party manager) script. Can I use it to retrieve stat data from all party members and display it in a stats ui menu that I created? 
+// Assume there are only up to four members. And that if there are less than 4, the screen for their stats is there its just blank
+// Can you update statsUImanager to toggle the stats menu off and on by clicking "O"
+// Can you automatically populate the parents for each UIStatEntry from the objects in the memberPrefabs list?
+
 using UnityEngine;
 using System.Collections.Generic;
 
