e26ae99bd2d04627054b98492637a3395cc8c706
dmpinyar <dmpinyard@gmail.com>
2026-04-16 17:10:39 -0400

    fixed all enemy interaction, still need to apply damage

--- Assets/Scripts/Towers/Tower3.cs
+++ Assets/Scripts/Towers/Tower3.cs
@@ -5,6 +5,7 @@ public class Tower3 : TowerParent
     public float stasisDuration;
     private float stasisTimer = 0;
     private GameObject heldEnemy;
+    private float oldSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start() {
@@ -17,12 +18,26 @@ public class Tower3 : TowerParent
             // hold enemy
             // not quite sure how the logic is going to work since enemies
             // haven't been added yet but whatever.
+            EnemyMovement movement = heldEnemy.GetComponent<EnemyMovement>();
+            if (movement != null && oldSpeed == 0) {
+                oldSpeed = movement.speed;
+                movement.speed = 0;
+            }
+                
+
+            if (stasisTimer > 0)
+                stasisTimer -= FRAME_TICK_CONSTANT;
+            else {
+                movement.speed = oldSpeed;
+                oldSpeed = 0;
+                heldEnemy = null;
+            }
 
         } else {
             if (recoil <= 0) {
-                GameObject nearerstEnemy = findNearestEnemy();
-                if ((nearerstEnemy.transform.position - transform.position).magnitude <= range) {
-                    heldEnemy = nearerstEnemy;
+                GameObject nearestEnemy = findNearestEnemy();
+                if (nearestEnemy != null && (nearestEnemy.transform.position - transform.position).magnitude <= range) {
+                    heldEnemy = nearestEnemy;
                     stasisTimer = stasisDuration + FRAME_TICK_CONSTANT;
                     recoil = fireDelay + FRAME_TICK_CONSTANT;
                 }
